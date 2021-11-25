ab6315e936287a4b8528e000edb8992f4e7caf82
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-11-25 13:07:40 +0700

    workshop #calculator

---
+++ firstapp/lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Calculator Application"),
+        ),
+        body: Home(),
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  // const Home({ Key? key }) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController quantity = TextEditingController();
+  TextEditingController price = TextEditingController();
+  // double price = 100;
+  TextEditingController result = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    result.text =
+        ("Buy apple x Kg., we have to pay x THB, Cause apple x per kg.");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(children: [
+              // Text("Picture" , style: TextStyle(fontFamily: "DSN" ,fontWeight: FontWeight.bold, fontStyle: FontStyle.italic ,color: Colors.red  , fontSize: 50)),
+              Image.asset("assets/red-apple-basket.jpg"),
+              SizedBox(
+                height: 20,
+              ),
+              Text("Calculate Program",
+                  style: TextStyle(
+                      fontFamily: "DSN",
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.red,
+                      fontSize: 50)),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: quantity,
+                decoration: InputDecoration(
+                    labelText: "Apple Amount", border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: price,
+                decoration: InputDecoration(
+                    labelText: "Apple Price", border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  var cal =
+                      double.parse(quantity.text) * double.parse(price.text);
+                  print("Apple quantity: ${quantity.text} Total: ${cal} THB");
+                  setState(() {
+                    result.text =
+                        "Buy apple ${quantity.text} Kg., we have to pay ${cal} THB, Cause apple ${price.text} per kg.";
+                  });
+                },
+                child: Text("Calculate"),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Color(0xffF6A294)),
+                  padding: MaterialStateProperty.all(
+                      EdgeInsets.fromLTRB(10, 10, 10, 10)),
+                  textStyle: MaterialStateProperty.all(TextStyle(
+                      fontFamily: "DSN", fontSize: 20, color: Colors.white)),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(result.text)
+            ]),
+          ),
+        ),
+      ],
+    );
+  }
+}
