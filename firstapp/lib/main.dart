75c229f3fce1f92beb26c738c01aaa90d2fe75cb
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-12-02 12:27:00 +0700

    Workshop 12-2-2021
    create bottom navigation bar

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:firstapp/pages/cal.dart';
+import 'package:firstapp/pages/contact.dart';
+import 'package:firstapp/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -5,8 +8,23 @@ void main(List<String> args) {
 }
 
 class MyApp extends StatelessWidget {
-  // const MyApp({ Key? key }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage()
+    );
+  }
+}
 
+class MainPage extends StatefulWidget {
+    const MainPage({Key? key}) : super(key: key);
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex =0;
+  final tabs = [HomePage(), CalculatePage(), ContactPage()];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,99 +32,22 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Calculator Application"),
         ),
-        body: Home(),
-      ),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  // const Home({ Key? key }) : super(key: key);
-
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  TextEditingController quantity = TextEditingController();
-  TextEditingController price = TextEditingController();
-  // double price = 100;
-  TextEditingController result = TextEditingController();
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    result.text =
-        ("Buy apple x Kg., we have to pay x THB, Cause apple x per kg.");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(children: [
-              // Text("Picture" , style: TextStyle(fontFamily: "DSN" ,fontWeight: FontWeight.bold, fontStyle: FontStyle.italic ,color: Colors.red  , fontSize: 50)),
-              Image.asset("assets/red-apple-basket.jpg"),
-              SizedBox(
-                height: 20,
-              ),
-              Text("Calculate Program",
-                  style: TextStyle(
-                      fontFamily: "DSN",
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                      color: Colors.red,
-                      fontSize: 50)),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                controller: quantity,
-                decoration: InputDecoration(
-                    labelText: "Apple Amount", border: OutlineInputBorder()),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                controller: price,
-                decoration: InputDecoration(
-                    labelText: "Apple Price", border: OutlineInputBorder()),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  var cal =
-                      double.parse(quantity.text) * double.parse(price.text);
-                  print("Apple quantity: ${quantity.text} Total: ${cal} THB");
-                  setState(() {
-                    result.text =
-                        "Buy apple ${quantity.text} Kg., we have to pay ${cal} THB, Cause apple ${price.text} per kg.";
-                  });
-                },
-                child: Text("Calculate"),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Color(0xffF6A294)),
-                  padding: MaterialStateProperty.all(
-                      EdgeInsets.fromLTRB(10, 10, 10, 10)),
-                  textStyle: MaterialStateProperty.all(TextStyle(
-                      fontFamily: "DSN", fontSize: 20, color: Colors.white)),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(result.text)
-            ]),
-          ),
+        body: tabs[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home) , label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.calculate) , label: "Calculate"),
+            BottomNavigationBarItem(icon: Icon(Icons.contact_mail) , label: "Contact")
+          ],
+          onTap: (index){
+            setState(() {
+              print(index);
+              _currentIndex = index;
+            });
+          },
         ),
-      ],
+      ),
     );
   }
 }
